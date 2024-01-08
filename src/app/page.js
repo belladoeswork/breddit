39fccad7b3a3215b09462e660af348b9ce89b47f
belladoeswork <belladoeswork@gmail.com>
2024-01-08 17:12:23 +0800

    final

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Home2 from "@/app/home/page";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div>
